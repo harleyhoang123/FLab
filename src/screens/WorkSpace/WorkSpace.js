510f9c265755272389484562b7b103446aac5a9d
Son Hoang Hai <frchoanghaison@gmail.com>
2022-11-03 14:33:02 +0700

    update-3/11/2022

--- src/screens/WorkSpace/WorkSpace.js
+++ src/screens/WorkSpace/WorkSpace.js
@@ -1,12 +1,17 @@
 import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
+import { StyleSheet, View, Text, Button, TextInput } from "react-native";
 import LabNavigator from "../../navigations/LabNavigator";
+import {
+  useFonts,
+  BeVietnamPro_200ExtraLight_Italic,
+} from "@expo-google-fonts/dev";
 import {
   DocumentEditorContainerComponent,
   Toolbar,
   DocumentEditorComponent,
   WordExport,
   SfdtExport,
+  SpreadSheetDocument,
 } from "@syncfusion/ej2-react-documenteditor";
 import "../../../node_modules/@syncfusion/ej2-base/styles/material.css";
 import "../../../node_modules/@syncfusion/ej2-buttons/styles/material.css";
@@ -19,28 +24,41 @@ import "../../../node_modules/@syncfusion/ej2-dropdowns/styles/material.css";
 import "../../../node_modules/@syncfusion/ej2-react-documenteditor/styles/material.css";
 
 DocumentEditorContainerComponent.Inject(Toolbar);
+let SSObj = SpreadSheetDocument;
 export default function WorkSpace() {
-  const save = () => {
+  const saveDoc = () => {
     SSObj.save({
-      url: "",
+      url: "'https://ej2services.syncfusion.com/production/web-services/api/spreadsheet/save'",
       filename: "sampledata",
       savetype: "pdf",
     });
+    console.log("click");
+    console.log(
+      JSON.stringify({
+        Name: document.getElementById("documentcontainer").value,
+      })
+    );
   };
   return (
     <View>
       <LabNavigator navigation={navigation} />
-      <View>
-        <View style={styles.container}>
+      <View style={styles.container}>
+        <View style={styles.left}>
+          <View style={styles.leftHeader}>
+            <Text style={styles.leftContent}>List WorkSpace</Text>
+          </View>
+        </View>
+        <View style={styles.right}>
+          <TextInput />
           <DocumentEditorContainerComponent
-            id="container"
+            id="documentcontainer"
             height={"auto"}
             serviceUrl="https://ej2services.syncfusion.com/production/web-services/api/documenteditor/"
             enableToolbar={true}
             allowSave={true}
-            saveUrl=""
+            saveUrl="https://ej2services.syncfusion.com/production/web-services/api/spreadsheet/save"
           />
-          <Button onClick={save} title="Save"></Button>
+          <Button onPress={saveDoc} title="Save"></Button>
         </View>
       </View>
     </View>
@@ -48,9 +66,32 @@ export default function WorkSpace() {
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  leftContent: {
+    paddingTop: 28,
+    paddingBottom: 28,
+    paddingRight: 8,
+    paddingLeft: 8,
+    borderWidth: 1,
+    borderColor: "#DEE2E6",
+    fontFamily: "sans-serif",
+    fontSize: 16,
+  },
   saveBtn: {
     height: 40,
     width: 40,
+  },
+  left: {
+    width: "18%",
+    height: "100%",
+    borderRadius: 3,
+    borderWidth: 1,
+    borderColor: "#DEE2E6",
+  },
+  right: {
+    width: "82%",
   },
 });
