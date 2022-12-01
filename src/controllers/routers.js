37bb10c9a8161c65fadfaabfe729a77e8dbf7473
Nguyen Cong Son <ncson8920@gmail.com>
2022-12-01 20:32:05 +0700

    assign api

--- src/controllers/routers.js
+++ src/controllers/routers.js
@@ -1,6 +1,6 @@
 export const routes = {
     authentication: {
-        login: '/capstone/authentication/public/api/v1/accounts/account/login',
+        login: 'http://192.168.31.197:8080/flab/authentication/public/api/v1/accounts/account/login',
         logout: '/users/logout',
     },
 };
